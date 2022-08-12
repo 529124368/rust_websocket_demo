8056c1a83b740a41dc544b772671606f44504dc5
529124368 <529124368@qq.com>
2022-08-12 12:04:50 +0900

    添加websocket客户端

--- examples/client.rs
+++ examples/client.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use futures_util::{future, pin_mut, StreamExt};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
